1fed43cd9e8b11f89ba07a8a4cdedcf9089637b6
Sagar Prasad <saprasad@csumb.edu>
2021-12-10 23:22:12 -0800

    movie detail html page

--- Movie_Randomizer/flask_app.py
+++ Movie_Randomizer/flask_app.py
@@ -18,48 +18,72 @@ bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'csumb-otter'
 
-my_key = 'ef955c2e'
+def searchAPI(title):
+  my_key = 'ef955c2e'
 
-# Random names that use to generate first 5 movies.
-deault_title = ["Battle", "End", "World", "War", "Sea", "Transform", "Blade", "Flower", "Dog", "Cat"]
-random_default_title = random.sample(deault_title, 1)
-print(random_default_title)
+  # Random names that use to generate first 5 movies.
+  deault_title = ["Battle", "End", "World", "War", "Sea", "Transform", "Avenger", "Flower", "Dog", "Cat", "Love"]
+  random_default_title = random.sample(deault_title, 1)
+  print(random_default_title)
 
-# USE https://www.omdbapi.com/ to see what parameter can be use.
-payload = {
+  if not title:
+    movie_title = random_default_title
+  else:
+    movie_title = title
+
+  print ("In search API")
+
+  # USE https://www.omdbapi.com/ to see what parameter can be use.
+  payload = {
+    'apikey': my_key,
+    's': movie_title,
+    'type': 'movie',
+  }
+
+  endpoint = "http://www.omdbapi.com/?"
+
+  # This 
+  try:
+    r = requests.get(endpoint, params=payload)
+    data = r.json()
+
+    movie_list = list(data['Search'])
+    shuffle_list = random.sample(movie_list,6)
+
+    return shuffle_list
+  except:
+    print('please try again')
+
+# searchAPI('')
+
+def callAPI(id):
+  my_key = 'ef955c2e'
+
+  print ("In call API")
+
+  payload = {
   'apikey': my_key,
-#   't': 'Iron Man'
-    's': random_default_title,
-  'type': 'movie',
-#   'page': 2
-}
+  'i': id,
+  'plot': 'full'
+  }
 
-endpoint = "http://www.omdbapi.com/?"
+  endpoint = "http://www.omdbapi.com/?"
 
-# This 
-try:
-  r = requests.get(endpoint, params=payload)
-  data = r.json()
+  # This 
+  try:
+    r = requests.get(endpoint, params=payload)
+    data = r.json()
 
-  movie_list = list(data['Search'])
-  shuffle_list = random.sample(movie_list,6)
-#   pprint(shuffle_list[0]['Poster'])
+    print (data['Ratings'][0]['Source'])
+    print (data['Ratings'][0]['Value'])
+    print (data['Plot'])
 
-#   print movie_list, np.random.shuffle(movie_list)
-#   pprint(movie_list)
-#   data = dict(movie_list)
-#   print(data['Search'][0])
-    # print(next(iter(data.values())))
-#   print(data['Title'])
-#   pprint(data['TotalResult'])
+    return data
+  except:
+    print('please try again')
 
-#   urllib.request.urlretrieve(shuffle_list[0]['Poster'], "poster_0.jpg")
-#   img = Image.open("poster_0.jpg")
-#   img.show()
-except:
-  print('please try again')
-
-
+  
+callAPI('tt3896198')
 
 class MovieList(FlaskForm):
     movie_title = StringField('Movie Title',
@@ -83,21 +107,16 @@ def home():
         title = form.movie_title.data
         return redirect('/detail')
 
+    shuffle_list = searchAPI('')
 
-    return render_template('home.html', form=form, image_data_0 = shuffle_list[0],
-                            image_data_1 = shuffle_list[1], image_data_2 = shuffle_list[2],
-                            image_data_3 = shuffle_list[3], image_data_4 = shuffle_list[4])
+    return render_template('home.html', form=form, image_data = shuffle_list)
 
 
 # Detail of specific movie when clicked (WIP)
-@app.route('/detail')
-def detail():
-    payload2 = {
-        'apikey': my_key,
-        's': title,
-        'type': 'movie',
-    }
-    r = requests.get(endpoint, params=payload2)
-    data = r.json()
-    print(data)
-    return render_template('detail.html', data=data)
+@app.route('/detail/<id>')
+def detail(id):
+
+  # print (id['Ratings']['Metascore'])
+  selected_id = callAPI(id)
+  print (selected_id['Ratings'])
+  return render_template('detail.html', selected_id = selected_id)
